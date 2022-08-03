f40152605ff6185f6eb11a96617e31d9ca5607ae
ashwi <ashwi@Ashwini>
2022-08-03 19:12:29 +0530

    First commit.....

---
+++ src/gitpackage/Test1.java
@@ -0,0 +1,27 @@
+package gitpackage;
+
+import com.vedha.sampleprograms.EvenOdd;
+
+public class Test1 {
+
+	int num=3;
+	public void even()
+	{
+		if(num%2==0)
+		{
+			System.out.println("is even number");
+			
+		}
+		else
+		{
+			System.out.println("is odd number");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test1 primeNumber=new Test1();
+primeNumber.even();
+	}
+
+
+}
