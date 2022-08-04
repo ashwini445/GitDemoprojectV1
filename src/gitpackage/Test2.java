c0dd973b968620a24aa1e9ca3bcebaba704f9749
ashwi <ashwi@Ashwini>
2022-08-04 11:23:06 +0530

    commiting test2 to seleniumbranch

---
+++ src/gitpackage/Test2.java
@@ -0,0 +1,23 @@
+package gitpackage;
+
+public class Test2 {
+	int num=3;
+	public void even()
+	{
+		if(num%2==0)
+		{
+			System.out.println("is even number");
+			
+		}
+		else
+		{
+			System.out.println("is odd number");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test1 primeNumber=new Test1();
+primeNumber.even();
+	}
+
+}
