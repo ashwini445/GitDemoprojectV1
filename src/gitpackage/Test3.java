b6400eb3b85a9186f107494ccbde2849bcf9e860
ashwi <ashwi@Ashwini>
2022-08-05 15:39:00 +0530

    added test3 java file

---
+++ src/gitpackage/Test3.java
@@ -0,0 +1,25 @@
+package gitpackage;
+
+public class Test3 {
+
+	int num=3;
+	int num1=4;
+	public void even()
+	{
+		if(num%2==0)
+		{
+			System.out.println("is even number");
+			
+		}
+		else
+		{
+			System.out.println("is odd number");
+		}
+		System.out.println("hi ashwini");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test1 primeNumber=new Test1();
+primeNumber.even();
+	}
+}
